136320d449d687a731c1c06a8d1ce3372a8390f0
The-Veba-Doctor <115376823+The-Veba-Doctor@users.noreply.github.com>
2022-11-13 17:33:05 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Welcome My Website</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <title>Welcome My Website 👋 !!</title>      </Head>
 
       <main>
         <Header title="Hello User" />
